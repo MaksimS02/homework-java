ac4dc4f460ee9fcf291e97f49c44dfa005cfe78b
MaksimS02 <sankov.m@yandex.ru>
2021-01-16 17:32:22 +0100

    russian text print

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,6 +1,6 @@
 public class MyFirstApp {
 	public static void main (String[] args) {
 		System.out.println("Hello, world");
-		System.out.println("I am a programmer");
+		System.out.println("я программист");
    }
 }
